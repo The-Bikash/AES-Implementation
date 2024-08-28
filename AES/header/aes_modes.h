a4d5ac5725b10e2697adef2632ef6db41ff5128f
Bikash Samanta <the-bikash@outlook.com>
2024-08-28 23:16:57 +0530

    update : aes_modes.h

--- AES/header/aes_modes.h
+++ AES/header/aes_modes.h
@@ -3,6 +3,7 @@
 
 #include "aes.h"
 #include <stdio.h>
+#include <time.h>
 
 Block128 pkcs7_pad(unsigned char* _Src, size_t _Len) {
     unsigned char _Remainder = _Len % 16;
@@ -26,10 +27,10 @@ size_t pkcs7_unpad(unsigned char* _Src, size_t _Len) {
     return _Len;
 }
 
-size_t AES_ECB_encrypt(void* _Dst, void* _Src, size_t _Size, void* _Key) {
+size_t AES_ECB_encrypt(void* _Dst, const void* _Src, size_t _Size, const void* _Key) {
     Block128* _DstPtr = (Block128*)_Dst;
-    Block128* _SrcPtr = (Block128*)_Src;
-    Block128* _KeyPtr = (Block128*)_Key;
+    const Block128* _SrcPtr = (const Block128*)_Src;
+    const Block128* _KeyPtr = (const Block128*)_Key;
 
     size_t _NewSize = _Size / 16;
     size_t i = 0;
@@ -39,15 +40,43 @@ size_t AES_ECB_encrypt(void* _Dst, void* _Src, size_t _Size, void* _Key) {
     return (i + 1) * 16;
 }
 
-size_t AES_ECB_decrypt(void* _Dst, void* _Src, size_t _Size, void* _Key) {
+size_t AES_ECB_decrypt(void* _Dst, const void* _Src, size_t _Size, const void* _Key) {
     Block128* _DstPtr = (Block128*)_Dst;
-    Block128* _SrcPtr = (Block128*)_Src;
-    Block128* _KeyPtr = (Block128*)_Key;
+    const Block128* _SrcPtr = (const Block128*)_Src;
+    const Block128* _KeyPtr = (const Block128*)_Key;
 
     size_t _NewSize = _Size / 16;
     for (size_t i = 0; i < _NewSize; ++i)
         _DstPtr[i] = _aes_decrypt(_SrcPtr[i], _KeyPtr);
     return pkcs7_unpad((unsigned char*)_Dst, _Size);
+}
+
+size_t AES_CBC_encrypt(void* _Dst, const void* _Src, size_t _Size, const void* _Key) {
+    Block128* _DstPtr = (Block128*)_Dst;
+    const Block128* _SrcPtr = (const Block128*)_Src;
+    const Block128* _KeyPtr = (const Block128*)_Key;
+
+    _Seed(clock());
+    *_DstPtr = random_message();
+    ++_DstPtr;
+    size_t _NewSize = _Size / 16, i = 0;
+    for (; i < _NewSize; ++i)
+        _DstPtr[i] = _aes_encrypt(_XorBlock128(_SrcPtr[i], *(_DstPtr + i - 1)), _KeyPtr);
+    _DstPtr[i] = _aes_encrypt(_XorBlock128(pkcs7_pad((unsigned char*)_Src, _Size), *(_DstPtr + i - 1)), _KeyPtr);
+    return (i + 2) * 16;
+}
+
+size_t AES_CBC_decrypt(void* _Dst, const void* _Src, size_t _Size, const void* _Key) {
+    Block128* _DstPtr = (Block128*)_Dst;
+    const Block128* _SrcPtr = (const Block128*)_Src;
+    const Block128* _KeyPtr = (const Block128*)_Key;
+
+    ++_SrcPtr;
+    size_t _NewSize = (_Size - 1) / 16;
+
+    for (size_t i = 0; i < _NewSize; ++i)
+        _DstPtr[i] = _XorBlock128(_aes_decrypt(_SrcPtr[i], _KeyPtr), *(_SrcPtr + i - 1));
+    return pkcs7_unpad((unsigned char*)_Dst, _Size - 1);
 }
 
 #endif
